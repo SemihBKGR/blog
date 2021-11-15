6850e89f44124be33866b0214ddf7486231feb68
SemihBG <buyukgungorsemih@gmail.com>
2021-11-15 23:58:54 +0300

    init spring boot maven project

---
+++ src/test/java/com/semihbkgr/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihbkgr.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
