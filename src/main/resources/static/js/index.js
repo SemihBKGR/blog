dab04d6017fae43a0a8cb7aff9a65f691bf0e794
SemihBG <buyukgungorsemih@gmail.com>
2021-11-20 03:05:39 +0300

    adds responsive multi column grid list in home page, review of home and subject pages

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,5 +3,16 @@ function convertUnixTimeToDate(unixTimestamp) {
     const year = date.getFullYear()
     const month = date.getMonth()
     const day = date.getDay()
-    return month + '/' + day + '/' + year
+    //return month + '/' + day + '/' + year
+    return '00/00/0000'
+}
+
+function addPadDiv(divName) {
+    const row = document.getElementsByClassName("row").item(0)
+    const padColDiv = document.createElement("div")
+    padColDiv.classList.add("col")
+    const padSubjectDiv = document.createElement("div")
+    padSubjectDiv.classList.add(divName)
+    padColDiv.appendChild(padSubjectDiv)
+    row.appendChild(padColDiv)
 }
