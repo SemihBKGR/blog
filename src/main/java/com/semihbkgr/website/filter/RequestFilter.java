6d23f9804e7575f5ae09d8e5aa6adf1d090c03d1
SemihBG <buyukgungorsemih@gmail.com>
2021-11-25 13:29:22 +0300

    review of sql and js files, adds file rolling for request logging

--- src/main/java/com/semihbkgr/website/filter/RequestFilter.java
+++ src/main/java/com/semihbkgr/website/filter/RequestFilter.java
@@ -8,12 +8,13 @@ import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Component
-@Slf4j
+@Slf4j(topic = "request")
 public class RequestFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        log.info("URI: {}", exchange.getRequest().getURI());
+        if (!exchange.getRequest().getPath().toString().startsWith("/static"))
+            log.info("URI: {}", exchange.getRequest().getURI());
         return chain.filter(exchange);
     }
 
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,9 @@
 function convertUnixTimeToDate(unixTimestamp) {
     const date = new Date(unixTimestamp * 1000)
+    console.log(date)
     const year = date.getFullYear()
-    const month = date.getMonth()
-    const day = date.getDay()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
     return month + '/' + day + '/' + year
 }
 
