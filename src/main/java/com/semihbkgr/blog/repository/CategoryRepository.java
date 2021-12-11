588f52a0f5898dd5f20f89601d6b6d0ac799ae5b
SemihBKGR <buyukgungorsemih@gmail.com>
2021-12-11 12:24:06 +0300

    review of pom.xml and html files, updates ssl properties

--- src/main/java/com/semihbkgr/blog/repository/CategoryRepository.java
+++ src/main/java/com/semihbkgr/blog/repository/CategoryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 @Repository
-public interface CategoryRepository extends R2dbcRepository<Category,Integer> {
+public interface CategoryRepository extends R2dbcRepository<Category, Integer> {
 
     @Query("SELECT DISTINCT `categories`.* FROM `posts` INNER JOIN `categories` ON `posts`.`category_id` = `categories`.`id` WHERE `posts`.`subject_id` = ? ORDER BY `categories`.`display_order`")
     Flux<Category> findAllBySubjectId(int subjectId);
